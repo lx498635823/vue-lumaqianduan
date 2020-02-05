d8cd167f3e431fc24a1e8988758dbd807ab68fc6
git <498635823@qq.com>
2020-02-05 22:57:44 +0800

    构建项目，配置全局样式和路由和elementui

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+//import Login from "../views/Login/index.vue";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    // 重定向直接在path后面添加，默认连接到这里
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    //component: Login
+    //或者这样写
+    component: () => import("../views/Login/index.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="login">123</div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+//scoped作用是只在当前文件有效
+#login {
+  background-color: #344a5f;
+  // vh单位代表当前页面可视区域窗口的百分比~！！
+  height: 100vh;
+}
+</style>
